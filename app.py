009d391ab645aecf4a9dd0bc7a8dac01b5994b84
Jon Blanchard <38008149+jfblanchard@users.noreply.github.com>
2023-01-03 11:40:09 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,9 @@ import streamlit as st
 st.write('Simple Slider that computes the cube of a value')
 x = st.slider('Select a value to cube')
 st.write(x, 'cubed is', x * x * x)
+
+if st.button('Say hello'):
+     st.write('Why hello there')
+else:
+     st.write('Goodbye')
+    
