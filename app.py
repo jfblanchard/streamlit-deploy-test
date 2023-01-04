696eafdbb3d5d26d9cc32a93134962cc432e633a
Jon Blanchard <38008149+jfblanchard@users.noreply.github.com>
2023-01-04 10:59:46 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import streamlit as st
+from fastai.vision.all import *
+import fastbook
+fastbook.setup_book()
 
 st.write('Simple Slider that computes the cube of a value')
 x = st.slider('Select a value to cube')
