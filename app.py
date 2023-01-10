f16b9fd04e41c204ae7b4a4cdf0c43af5c7a0075
Jon Blanchard <38008149+jfblanchard@users.noreply.github.com>
2023-01-10 13:39:38 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 from PIL import Image
-# from fastai.vision.all import *
-# import fastbook
-# fastbook.setup_book()
+from fastai.vision.all import *
+import fastbook
+fastbook.setup_book()
 
-# path = untar_data(URLs.PETS)/'./images'
+path = untar_data(URLs.PETS)/'./images'
 
 # should be in current directory
 with open('./test.txt','r') as f:
@@ -16,15 +16,15 @@ st.write(test_text)
 def is_cat(x): 
     return x[0].isupper()
 
-# def train_image_classifier(path):
-#      dls = ImageDataLoaders.from_name_func(
-#          path, get_image_files(path), valid_pct=0.2, seed=42,
-#          label_func=is_cat, item_tfms=Resize(224))
+def train_image_classifier(path):
+     dls = ImageDataLoaders.from_name_func(
+         path, get_image_files(path), valid_pct=0.2, seed=42,
+         label_func=is_cat, item_tfms=Resize(224))
 
-#      learn = vision_learner(dls, resnet34, metrics=error_rate)
-#      learn.fine_tune(1)
+     learn = vision_learner(dls, resnet34, metrics=error_rate)
+     learn.fine_tune(1)
      
-#      return learn
+     return learn
 
 # Create UI
 st.markdown('<h2><center>Cat/Dog Image Classifier</center></h2>', unsafe_allow_html=True)  # or try st.header()
@@ -36,12 +36,12 @@ if uploaded_file is not None:
     st.image(image, caption='Uploaded image')
 
     st.write('Training...')  # later use tqdm.... Or load the pretrained model
-    # learn = train_image_classifier(path)
-    # st.write('Done Training')
+    learn = train_image_classifier(path)
+    st.write('Done Training')
 
-    # is_cat,_,probs = learn.predict(image)
-    # st.write(f"Is this a cat?: {is_cat}.")
-    # st.write(f"Probability it's a cat: {probs[1].item():.6f}")
+    is_cat,_,probs = learn.predict(image)
+    st.write(f"Is this a cat?: {is_cat}.")
+    st.write(f"Probability it's a cat: {probs[1].item():.6f}")
 
 st.write('Have a great day!')
 # Other gui stuff
