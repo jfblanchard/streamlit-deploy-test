6e515301d136f8f46f55f530d9c1689dd913dedc
jfblanchard <jonfblanchard@protonmail.com>
2023-12-05 12:17:05 -0700

    Simplified app.py

--- app.py
+++ app.py
@@ -1,56 +1,14 @@
+# More basic version of a streamlit app, just to verify functionality
+# other version with neural net errors out during inference.
 import streamlit as st
-from PIL import Image
-from fastai.vision.all import *
-import fastbook
-fastbook.setup_book()
+import pandas as pd
 
-path = untar_data(URLs.PETS)/'./images'
+st.title('Streamlit app test2')
 
-# should be in current directory
-with open('./test.txt','r') as f:
-    test_text = f.read()
-    
-st.write(test_text)
+# write a dataframe and view it
+df = pd.DataFrame({"one": [1, 2, 3], "two": [4, 5, 6], "three": [7, 8, 9]})
+st.write(df)
 
-# functions
-def is_cat(x): 
-    return x[0].isupper()
 
-def train_image_classifier(path):
-     dls = ImageDataLoaders.from_name_func(
-         path, get_image_files(path), valid_pct=0.2, seed=42,
-         label_func=is_cat, item_tfms=Resize(224))
 
-     learn = vision_learner(dls, resnet34, metrics=error_rate)
-     learn.fine_tune(1)
-     
-     return learn
-
-# Create UI
-st.markdown('<h2><center>Cat/Dog Image Classifier</center></h2>', unsafe_allow_html=True)  # or try st.header()
-uploaded_file = st.file_uploader("Choose an image file of a cat or dog")
-
-# only shows up when a file is uploaded
-if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    st.image(image, caption='Uploaded image')
-
-    st.write('Training...')  # later use tqdm.... Or load the pretrained model
-    learn = train_image_classifier(path)
-    st.write('Done Training')
-
-    is_cat,_,probs = learn.predict(image)
-    st.write(f"Is this a cat?: {is_cat}.")
-    st.write(f"Probability it's a cat: {probs[1].item():.6f}")
-
-st.write('Have a great day!')
-# Other gui stuff
-# st.write('Simple Slider that computes the cube of a value')
-# x = st.slider('Select a value to cube')
-# st.write(x, 'cubed is', x * x * x)
-
-# if st.button('Say hello'):
-#      st.write('Why hello there')
-# else:
-#      st.write('Goodbye')
-    
+#%%
